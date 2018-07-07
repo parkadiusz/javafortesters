69abb91e843f08f8f37d8c650342c1b0be42e6c6
parkadiusz <arkadiusz.purski@onet.pl>
2018-07-07 16:07:45 +0200

    Added some function

--- sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
@@ -3,7 +3,25 @@ package ru.stqa.pft.sandbox;
 public class MyFirstProgram {
 
 	public static void main (String[] args ) {
-		System.out.println("Hello, World!");
+	  hello("Arek");
+	  hello("Alexei");
 
+	  double l = 5;
+    System.out.println("Powierzchnia o boku " + l + " = " + area(l) );
+
+    double a = 4, b=5;
+    System.out.println("Powierzchnia prostokata o bokach " + a + " oraz " + b + " = " + area(a,b));
 	}
+
+	public static void hello(String somebody){
+    System.out.println("Hello, World! " + somebody );
+  }
+
+  public static double area(double len){
+	  return len*len;
+  }
+
+  public static double area(double a, double b){
+	  return a*b;
+  }
 }
